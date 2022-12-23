4d83b22c8068b4a1f2f90bb549911f11b02685f6
Gogy <thegogy@proton.me>
2022-12-23 14:40:01 +0000

    fix loading arrays

--- test_scripts/solve_multiple.py
+++ test_scripts/solve_multiple.py
@@ -1,4 +1,5 @@
 import numpy as np
+from alive_progress import alive_bar
 from time import process_time, perf_counter
 from test_scripts.tests import test_sudoku
 
@@ -20,28 +21,27 @@ def solve_multiple(sudoku_array: np.ndarray,
         quit_after (int) : The number of sudokus to stop processing after.
     '''
     total_time = 0
-    count = 0
     total_count = len(sudoku_array) if quit_after == -1 else quit_after
     fastest = 10000
     slowest = 0
     solutions = np.ndarray
     script_start_time = perf_counter()
+    array_to_solve = sudoku_array if quit_after == -1 else sudoku_array[:quit_after]
     try:
-        for sudoku in sudoku_array:
-            print(f"SOLVING SUDOKU    {count}/{total_count}", end="\r")
-            count += 1
-            if count == quit_after: break
+        with alive_bar(total_count) as bar:
+            for sudoku in array_to_solve:
 
-            your_solution, time_taken = test_sudoku(sudoku, use_process_time)
+                your_solution, time_taken = test_sudoku(sudoku, use_process_time)
 
-            total_time += time_taken
-            if time_taken < fastest:
-                fastest = time_taken
-            if time_taken > slowest:
-                slowest = time_taken
+                total_time += time_taken
+                if time_taken < fastest:
+                    fastest = time_taken
+                if time_taken > slowest:
+                    slowest = time_taken
 
-            if save_to_file:
-                solutions = np.append(solutions, solution)
+                if save_to_file:
+                    solutions = np.append(solutions, solution)
+                bar()
 
     except KeyboardInterrupt:
         pass
@@ -49,10 +49,10 @@ def solve_multiple(sudoku_array: np.ndarray,
     print( f'''
 # - - - - - - - - - - - - - - - - - - - - - - - #
 
-    Solved {count} sudokus in {round(script_end_time - script_start_time, 3)} seconds.
+    Solved {total_count} sudokus in {round(script_end_time - script_start_time, 3)} seconds.
 
     TOTAL TIME                {total_time / 1000} s
-    SUDOKUS SOLVED            {count}
+    SUDOKUS SOLVED            {total_count}
 
     FASTEST TIME              {fastest} ms
     SLOWEST TIME              {slowest} ms
