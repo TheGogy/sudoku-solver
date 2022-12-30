7cca8f76883d60f56986c933e12c07a84098e8df
Gogy <thegogy@proton.me>
2022-12-29 21:23:18 -0800

    add timeout

--- test_scripts/tests.py
+++ test_scripts/tests.py
@@ -1,23 +1,42 @@
-from numpy import ndarray
+from numpy import ndarray, full
 from time import process_time, perf_counter
 from copy import deepcopy
+
+from signal import signal, alarm, SIGALRM
 
 from sudoku import sudoku_solver
 from test_scripts.utils import *
 
+TIMEOUT_THRESHOLD = 20
 
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(signum, frame):
+    print(u'\033[41m \033[01m \033[30mSolver timed out \033[0m')
+    raise TimeoutException
 
 def test_sudoku(sudoku: ndarray, use_process_time: bool):
+
+    signal(SIGALRM, timeout_handler)
+    alarm(TIMEOUT_THRESHOLD)
+
     # - - - - - - - - - - - - - - - - - - - #
-    # This code is written like this because if it was cleaner it has an impact on performance
-    if use_process_time:
-        start_time = process_time()
-        your_solution = sudoku_solver(sudoku)
-        end_time = process_time()
-    else:
-        start_time = perf_counter()
-        your_solution = sudoku_solver(sudoku)
-        end_time = perf_counter()
+    # This code is written like this because if it was cleaner it
+    # has an impact on performance
+    try:
+
+        if use_process_time:
+            start_time = process_time()
+            your_solution = sudoku_solver(sudoku)
+            end_time = process_time()
+        else:
+            start_time = perf_counter()
+            your_solution = sudoku_solver(sudoku)
+            end_time = perf_counter()
+
+    except TimeoutException:
+        return full((9,9), fill_value=0), TIMEOUT_THRESHOLD * 1000
     # - - - - - - - - - - - - - - - - - - - #
 
     return your_solution, (end_time-start_time) * 1000
@@ -25,7 +44,15 @@ def test_sudoku(sudoku: ndarray, use_process_time: bool):
 def test_sudoku_verbose(sudoku: ndarray, save_to_file: str or None, use_process_time: bool):
     sudoku_copy = deepcopy(sudoku)
 
-    solution, time_taken = test_sudoku(sudoku, use_process_time)
+    signal(SIGALRM, timeout_handler)
+    alarm(TIMEOUT_THRESHOLD)
+
+    try:
+        solution, time_taken = test_sudoku(sudoku, use_process_time)
+
+    except TimeoutException:
+        solution = full((9,9), fill_value=0)
+        time_taken = TIMEOUT_THRESHOLD
 
     print(" - - - - - - - - [ Input Sudoku ] - - - - - - - - ")
     print_sudoku(sudoku_copy)
